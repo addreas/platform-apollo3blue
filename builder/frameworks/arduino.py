993846dd10c65ce636ab498e8714b34d93197a86
Andreas Mårtensson <andreas@addem.se>
2020-11-01 16:22:30 +0100

    Compiles without linking

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -1,216 +1,63 @@
-# platform-apollo3blue: Apollo3Blue development platform for platformio.
-# Copyright 2019-present NigelB
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from os.path import isdir, join
+from os.path import isdir, isfile, join
 
 from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
-build_mcu = env.get("BOARD_MCU", board.get("build.mcu", ""))
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoapollo3")
-assert isdir(FRAMEWORK_DIR)
-
-CORE_DIR = join(FRAMEWORK_DIR, "cores", "arduino")
-SDK_DIR = join(CORE_DIR, "am_sdk_ap3")
-CMSIS_DIR = join(SDK_DIR, "CMSIS")
-THIRD_PARTY_DIR = join(SDK_DIR, "third_party")
-EXACTLE_DIR = join(THIRD_PARTY_DIR, "exactle")
-LIBRARY_DIR = join(FRAMEWORK_DIR, "libraries")
-
-VARIANTS_DIR = join(FRAMEWORK_DIR, "variants")
-BOARD_VARIANTS_DIR = join(VARIANTS_DIR, board.get("build.variant"))
+FRAMEWORK_DIR = platform.get_package_dir('framework-arduinoapollo3')
+VARIANT_DIR = join(FRAMEWORK_DIR, 'variants', board.get('build.variant'))
+CORES_DIR = join(FRAMEWORK_DIR, 'cores')
 
 env.Append(
-    ASFLAGS=[
-        "-c", "-g", "-MMD",
-        "-x", "assembler-with-cpp",
+    ASFLAGS=[f'@{join(VARIANT_DIR, "mbed", ".asm-flags")}',
+             f'@{join(VARIANT_DIR, "mbed", ".asm-symbols")}',
+             f'@{join(VARIANT_DIR, "mbed", ".includes")}'],
+    CCFLAGS=['-include',
+             join(VARIANT_DIR, 'mbed', 'mbed_config.h'),
+             '-include',
+             join(CORES_DIR, 'arduino', 'sdk', 'ArduinoSDK.h'),
+             '-iprefix',
+             f'{CORES_DIR}/',
+             f'@{join(VARIANT_DIR, "mbed", ".includes")}'],
+    CFLAGS=[f'@{join(VARIANT_DIR, "mbed", ".c-flags")}',
+            f'@{join(VARIANT_DIR, "mbed", ".c-symbols")}'],
+    CXXFLAGS=[f'@{join(VARIANT_DIR, "mbed", ".cxx-flags")}',
+              f'@{join(VARIANT_DIR, "mbed", ".cxx-symbols")}'],
+    LINKFLAGS=[# '-Wl,-Map,{build.path}/{build.project_name}.map', # ???
+               # f'{join(VARIANT_DIR, "mbed/libmbed-os")}.a', # covered by VARIANT_DIR in LIBPATH?
+               '--specs=nano.specs',
+               '$_CPPDEFFLAGS', # redundant?
+               f'@{join(VARIANT_DIR, "mbed", ".ld-flags")}',
+               f'@{join(VARIANT_DIR, "mbed", ".ld-symbols")}'],
 
+    LIBPATH=[
+        VARIANT_DIR,
+        join(VARIANT_DIR, 'libs')
     ],
 
-    CFLAGS=[
-        "-std=gnu11",
-        "--function-sections", "-mfpu=fpv4-sp-d16", "-Wall"
-    ],
+    LIBSOURCE_DIRS=[join(FRAMEWORK_DIR, 'libraries')],
 
-    CPPFLAGS=[
-        "-c", "-g", "-MMD",
-        "-mthumb",
-        "-mfloat-abi=hard",
-        "-fdata-sections",
-        "-Os",
-        "-ffunction-sections",
-
-        "-nostdlib",
-        "--param", "max-inline-insns-single=500",
-
-        "-fno-exceptions",
-        "-mcpu=%s" % board.get("build.cpu")
-    ],
-
-    CXXFLAGS=[
-        "-std=gnu++11",
-        "-fno-threadsafe-statics",
-        "-fno-rtti",
-    ],
+    LIBS=[], # ??
 
     CPPDEFINES=[
-        ("F_CPU", "$BOARD_F_CPU"),
-        ("ARDUINO", "10809"),
+        ('ARDUINO', 10810),
+        f'ARDUINO_{board.get("build.board")}',
+        'ARDUINO_ARCH_MBED',
+        f'ARDUINO_ARCH_{board.get("build.arch")}',
+        'MBED_NO_GLOBAL_USING_DIRECTIVE',
+        'CORDIO_ZERO_COPY_HCI',
     ],
 
     CPPPATH=[
-        join(BOARD_VARIANTS_DIR, "config"),
-        join(BOARD_VARIANTS_DIR, "bsp"),
-        join(CORE_DIR, "ard_sup"),
-        join(CORE_DIR, "ard_sup", "ard_supers"),
-        join(CORE_DIR),
-        join(VARIANTS_DIR, board.get("build.variant")),
-        join(SDK_DIR, "mcu", "apollo3"),
-        join(SDK_DIR, "mcu", "apollo3", "hal"),
-        join(SDK_DIR, "mcu", "apollo3", "regs"),
-        join(SDK_DIR, "utils"),
-        join(SDK_DIR, "devices"),
-        join(CMSIS_DIR, "AmbiqMicro", "Include"),
-        join(CMSIS_DIR, "ARM", "Include"),
-        join(THIRD_PARTY_DIR, "uecc", "micro-ecc"),
-        join(EXACTLE_DIR, "sw", "hci", "ambiq"),
-        join(EXACTLE_DIR, "sw", "hci", "include"),
-        join(EXACTLE_DIR, "sw", "hci", "ambiq", "apollo3"),
-        join(EXACTLE_DIR, "sw", "apps", "app"),
-        join(EXACTLE_DIR, "sw", "apps", "app", "include"),
-        join(EXACTLE_DIR, "sw", "services"),
-        join(EXACTLE_DIR, "sw", "stack", "hci"),
-        join(EXACTLE_DIR, "sw", "stack", "cfg"),
-        join(EXACTLE_DIR, "sw", "sec", "include"),
-        join(EXACTLE_DIR, "sw", "sec", "common"),
-        join(EXACTLE_DIR, "sw", "services"),
-        join(EXACTLE_DIR, "ws-core", "include"),
-        join(EXACTLE_DIR, "ws-core", "sw", "util"),
-        join(EXACTLE_DIR, "ws-core", "sw", "wsf", "ambiq"),
-        join(EXACTLE_DIR, "ws-core", "sw", "wsf", "include"),
-        join(EXACTLE_DIR, "sw", "stack", "include"),
-        join(EXACTLE_DIR, "sw", "profiles"),
-        join(EXACTLE_DIR, "sw", "profiles", "gatt"),
-        join(EXACTLE_DIR, "sw", "profiles", "gap"),
-
-        join(LIBRARY_DIR, "EEPROM", "src"),
-        join(LIBRARY_DIR, "PDM", "src"),
-        join(LIBRARY_DIR, "RTC", "src"),
-        join(LIBRARY_DIR, "Servo", "src"),
-        join(LIBRARY_DIR, "SoftwareSerial", "src"),
-        join(LIBRARY_DIR, "SPI", "src"),
-        join(LIBRARY_DIR, "Wire", "src"),
-
+        VARIANT_DIR,
+        join(CORES_DIR, 'mbed-os'),
+        join(CORES_DIR, 'arduino'),
+        join(CORES_DIR, 'arduino', 'mbed-bridge'),
+        join(CORES_DIR, 'arduino', 'mbed-bridge', 'core-api'),
     ],
-
-    LINKFLAGS=[
-        "-T%s" % join(VARIANTS_DIR, board.get("build.variant"), "linker_scripts", "gcc", board.get("build.linker_script")),
-        # "-Os",
-        "-mthumb",
-        "-mcpu=%s" % board.get("build.cpu"),
-        "-mfpu=fpv4-sp-d16",
-        "-mfloat-abi=hard",
-        "--specs=nano.specs",
-        "-mfloat-abi=hard",
-        "-nostdlib",
-        "-fno-exceptions",
-        "-static",
-        "-Wl,--gc-sections,--entry,Reset_Handler",
-        "-Wl,--check-sections",
-        "-Wl,--unresolved-symbols=report-all",
-        "-Wl,--warn-common",
-        "-Wl,--warn-section-align",
-
-        "-Wl,-Map=%s" % join("$BUILD_DIR", "program.map")
-    ],
-
-    LIBS=["m", "arm_cortexM4lf_math", "gcc", "stdc++", "nosys", "c"],
-
-    LIBPATH=[
-        join(VARIANTS_DIR, board.get("build.variant")),
-        join(CMSIS_DIR, "ARM", "Lib", "ARM")
-    ]
 )
 
-libs = []
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "FrameworkArduino"),
-    join(CORE_DIR, "ard_sup"),
-
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "apollo3_sdk_mcu"),
-    join(SDK_DIR, "mcu"),
-    # join(SDK_DIR, "devices")]
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "apollo3_sdk_devices"),
-    join(SDK_DIR, "devices"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "apollo3_sdk_utils"),
-    join(SDK_DIR, "utils"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "variant"),
-    join(BOARD_VARIANTS_DIR),
-
-))
-
-# Libraries
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "EEPROM"),
-    join(LIBRARY_DIR, "EEPROM", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "PDM"),
-    join(LIBRARY_DIR, "PDM", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "RTC"),
-    join(LIBRARY_DIR, "RTC", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "Servo"),
-    join(LIBRARY_DIR, "Servo", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "SoftwareSerial"),
-    join(LIBRARY_DIR, "SoftwareSerial", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "SPI"),
-    join(LIBRARY_DIR, "SPI", "src"),
-))
-
-libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "Wire"),
-    join(LIBRARY_DIR, "Wire", "src"),
-))
-
-env.Prepend(LIBS=libs)
+env.Prepend(_LIBFLAGS="-Wl,--whole-archive ")
+env.Append(_LIBFLAGS=" -Wl,--no-whole-archive -lstdc++ -lsupc++ -lm")
--- builder/main.py
+++ builder/main.py
@@ -1,118 +1,188 @@
 # platform-apollo3blue: Apollo3Blue development platform for platformio.
 # Copyright 2019-present NigelB
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os.path import join, isdir
-from SCons.Script import AlwaysBuild, Builder, Default, DefaultEnvironment
-import platform as sys_pf
+
+import sys
+from os import makedirs
+from os.path import isdir, join, basename
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
+
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
 
-upload_protocol = env.subst("$UPLOAD_PROTOCOL")
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoapollo3")
-assert isdir(FRAMEWORK_DIR)
-
-if upload_protocol.startswith("svl"):
-    upload_program = join(FRAMEWORK_DIR, "tools", "artemis", sys_pf.system().lower(), "artemis_svl")
-    if sys_pf.system() == "Windows":
-        upload_program += ".exe"
-
-    elif sys_pf.system().lower() in ["darwin"]:
-        upload_program = join(FRAMEWORK_DIR, "tools", "artemis", "macosx", "artemis_svl")
-
-upload_speed = env.subst("$UPLOAD_SPEED")
-if len(upload_speed) == 0:
-    upload_speed = "921600"
-
-upload_port = env.subst("$UPLOAD_PORT")
-if len(upload_port) == 0:
-    env.AutodetectUploadPort()
-    
-    # env.Replace(
-    #     UPLOADER=upload_program,
-    #     UPLOADERFLAGS=[
-    #         "$UPLOAD_PORT",
-    #         "-b", "921600",
-    #         "-f", "$SOURCES",
-    #         "-v",
-    #     ],
-    #     UPLOADCMD="$UPLOADER $UPLOADERFLAGS"
-    # )
-
-# A full list with the available variables
-# http://www.scons.org/doc/production/HTML/scons-user.html#app-variables
 env.Replace(
-    AR="arm-none-eabi-ar",
-    AS="arm-none-eabi-as",
-    CC="arm-none-eabi-gcc",
-    CXX="arm-none-eabi-g++",
-    GDB="arm-none-eabi-gdb",
-    OBJCOPY="arm-none-eabi-objcopy",
-    RANLIB="arm-none-eabi-ranlib",
-    SIZETOOL="arm-none-eabi-size",
+    AR='arm-none-eabi-ar',
+    AS='arm-none-eabi-as',
+    CC='arm-none-eabi-gcc',
+    CXX='arm-none-eabi-g++',
+    GDB='arm-none-eabi-gdb',
+    OBJCOPY='arm-none-eabi-objcopy',
+    RANLIB='arm-none-eabi-ranlib',
+    SIZETOOL='arm-none-eabi-size',
 
-    ARFLAGS=["rc"],
+    ARFLAGS=['rc'], # rcsP in platform.txt
 
-    UPLOADER=upload_program,
-    UPLOADERFLAGS=[
-        "$UPLOAD_PORT",
-        "-b", upload_speed,
-        "-f", "$SOURCES",
-        "-v",
-    ],
-    UPLOADCMD="$UPLOADER $UPLOADERFLAGS"
+    SIZEPROGREGEXP=r'^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx)\s+(\d+).*', # different from platform.txt
+    SIZEDATAREGEXP=r'^(?:\.data|\.bss|\.noinit)\s+(\d+).*',
+    SIZECHECKCMD='$SIZETOOL -A -d $SOURCES',
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGSUFFIX='.axf'
 )
 
 env.Append(
-    ARFLAGS=[],
-    ASFLAGS=[],
-    CCFLAGS=[],
-    CXXFLAGS=["-std=gnu++11"],
-    LINKFLAGS=[],
-    CPPDEFINES=["PART_apollo3", "AM_PACKAGE_BGA", "AM_PART_APOLLO3", "ARDUINO_ARCH_APOLLO3"],
-    LIBS=[],
     BUILDERS=dict(
-        ElfToBin=Builder(
-            action=" ".join([
-                "$OBJCOPY",
-                "-O",
-                "binary",
-                "$SOURCES",
-                "$TARGET"]),
-            suffix=".bin"
+        AxfToBin=Builder(
+            action=env.VerboseAction(' '.join([
+                '$OBJCOPY',
+                '-O',
+                'binary',
+                '$SOURCES',
+                '$TARGET'
+            ]), 'Building $TARGET'),
+            suffix='.bin'
+        ),
+        AxfToHex=Builder(
+            action=env.VerboseAction(' '.join([
+                '$OBJCOPY',
+                '-O',
+                'ihex',
+                '$SOURCES',
+                '$TARGET'
+            ]), 'Building $TARGET'),
+            suffix='.hex'
         )
     )
 )
 
-#
-# Target: Build executable and linkable firmware
-#
-target_elf = env.BuildProgram()
+if 'nobuild' in COMMAND_LINE_TARGETS:
+    target_axf = join('$BUILD_DIR', '${PROGNAME}.axf')
+    target_bin = join('$BUILD_DIR', '${PROGNAME}.bin')
+else:
+    target_axf = env.BuildProgram()
+    target_bin = env.AxfToBin(join('$BUILD_DIR', '${PROGNAME}'), target_axf)
 
-#
-# Target: Build the .bin file
-#
-target_bin = env.ElfToBin(join("$BUILD_DIR", "firmware"), target_elf)
 
-#
-# Target: Upload firmware
-#
-upload = env.Alias(["upload"], target_bin, "$UPLOADCMD")
-AlwaysBuild(upload)
+AlwaysBuild(env.Alias('nobuild', target_axf))
+target_buildprog = env.Alias('buildprog', target_axf, target_axf)
 
-#
-# Target: Define targets
-#
-Default(target_bin)
+target_size = env.AddPlatformTarget(
+    'size',
+    target_axf,
+    env.VerboseAction('$SIZEPRINTCMD', 'Calculating size $SOURCE'),
+    'Program Size',
+    'Calculate program size',
+)
+
+FRAMEWORK_DIR = platform.get_package_dir('framework-arduinoapollo3')
+
+arduino_os_name = {
+    'win32': 'windows',
+    'linux': 'linux',
+    'darwin': 'macosx'
+}[sys.platform.lower()]
+
+upload_protocol = env.subst('$UPLOAD_PROTOCOL')
+
+def upload_program(protocol):
+    program = join(FRAMEWORK_DIR, 'tools', 'uploaders', protocol, 'dist', arduino_os_name, protocol)
+    if arduino_os_name == 'windows':
+        program += '.exe'
+
+    return program
+
+asb_flags = [
+    '--load-address-blob',
+    '0x20000',
+    '--magic-num',
+    '0xCB',
+    '-o',
+    join('$BUILD_DIR', 'asb_intermediate'),
+    '--version',
+    '0x0',
+    '--load-address-wired',
+    '0xC000',
+    '-i',
+    '6',
+    '--options',
+    '0x1',
+    '-b',
+    115200,
+    '-port',
+    '$UPLOAD_PORT',
+    '-r',
+    '2',
+    '-v',
+]
+
+upload_actions = [
+    env.VerboseAction(lambda: env.AutodetectUploadPort(), 'Looking for upload port...'),
+    env.VerboseAction(lambda: env.FlushSerialBuffer('$UPLOAD_PORT'), 'Toggling DTR/RTS...'), # DTR/RTS off, 0.1 sec, DTR/RTS on
+    # env.VerboseAction(lambda: env.TouchSerialPort('$UPLOAD_PORT', 1200), 'Poking serial port.'),  # open/close serial at 1200 baud
+    env.VerboseAction('$UPLOADCMD', 'Uploading $SOURCE')
+]
+
+upload_bauds = {
+    'svl': [921600, 460800, 230400, 115200, 57600],
+    'asb': [115200]
+}
+
+if not env.subst('$UPLOAD_SPEED'):
+    env.Replace(UPLOAD_SPEED=upload_bauds[upload_protocol][0])
+else:
+    assert int(env.subst('$UPLOAD_SPEED')) in upload_bauds[upload_protocol]
+
+if upload_protocol == 'svl':
+    env.Replace(
+        UPLOADER=upload_program('svl'),
+        UPLOADERFLAGS=['$UPLOAD_PORT', '-b', '$UPLOAD_SPEED', '-v'],
+        UPLOADCMD='"$UPLOADER" $UPLOADERFLAGS -f $SOURCE',
+
+        BOOTLOADER_UPLOADER=upload_program('asb'),
+        BOOTLOADER_UPLOADERFLAGS=asb_flags,
+        BOOTLOADER_SOURCE=join(FRAMEWORK_DIR, 'tools/uploaders/svl/bootloader/gcc/artemis_module/bin/svl.bin'),
+
+        LDSCRIPT_PATH=join(FRAMEWORK_DIR, 'tools/uploaders/svl/0x10000.ld'),
+    )
+    env.AddPlatformTarget(
+        'bootloader',
+        None,
+        [
+            env.VerboseAction(
+                '$BOOTLOADER_UPLOADER $BOOTLOADER_UPLOADERFLAGS --bin "$BOOTLOADER_SOURCE"',
+                'Burning bootloader',
+            ),
+        ],
+        'Burn Bootloader',
+    )
+elif upload_protocol == 'asb':
+    env.Default(UPLOAD_SPEED=115200)
+    env.Replace(
+        UPLOADER=upload_program('asb'),
+        UPLOADERFLAGS=asb_flags,
+        UPLOADCMD='"$UPLOADER" $UPLOADERFLAGS --bin $SOURCE',
+
+        LDSCRIPT_PATH=join(FRAMEWORK_DIR, 'tools/uploaders/asb/0xC000.ld'),
+    )
+else:
+    sys.stderr.write(f'Warning! Unknown upload protocol {upload_protocol}\n')
+
+env.AddPlatformTarget('upload', target_bin, upload_actions, 'Upload')
+
+
+Default([target_buildprog, target_size])
